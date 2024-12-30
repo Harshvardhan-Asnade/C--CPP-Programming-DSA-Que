c24d3b7224a13b466d26d560fb8d3d29c5a8a007
Harshvardhan-Asnade <harshasnade1@gmail.com>
2024-12-30 23:27:35 +0530

    2

---
+++ 01_USINGCOUNT.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello Harsh !"<<"\n"<<"How are you ?"; //<< we can use mutpily yime  
+  return 0;
+}
---
+++ 02_Printno.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "4+3" << "\n"; 
+    cout << 4 + 3;
+    return 0;
+}
