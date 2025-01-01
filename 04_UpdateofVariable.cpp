18305a7edcc662f9b3ceda1577668a584003bd66
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-01 23:37:54 +0530

    2

---
+++ 04_UpdateofVariable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 1;
+    cout << x << endl;
+    x = 8; // update
+    cout << x << "\n";
+    x = 24; // update
+    cout << x << "\n";
+}
