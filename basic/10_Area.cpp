748756aa261d7ae4592e3c39128216794b0430e1
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-03 23:15:30 +0530

    a

--- basic/10_Area.cpp
+++ basic/10_Area.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main()
 {
-    
+    int a = 7, b = 3, pie = 3.14, rectangle, circle, square;
+    // area
+    rectangle = a * b;
+    circle = pie * a * a;
+    square = a * a;
 }
