3c9fdf20270aaa6695726c4cdb340409fb92da63
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-05 22:55:39 +0530

    Update

---
+++ basic/15_TakeInputSumOF2Number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter the valve of x : ";
+    cin >> x;
+    cout << "Enter the valve of y : ";
+    cin >> y;
+    int sum = x + y;
+    cout << "The sum of x and y is : " << sum;
+}
