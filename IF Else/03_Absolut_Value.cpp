7a5aedcecc3e8819273541fc8d02cb50b1c0c3f7
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-18 14:37:43 +0530

    03

---
+++ IF Else/03_Absolut_Value.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << " Enter A Number : ";
+    cin >> a;
+    if (a>0)
+    {
+        cout << a;
+    }
+    if (a<0)
+    {
+        cout << -a;
+    }
+ 
+
+    return 0;
+}
