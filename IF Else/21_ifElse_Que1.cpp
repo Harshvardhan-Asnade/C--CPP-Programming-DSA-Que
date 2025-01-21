8a43f93685ef0491d5b6dd767d43d4d3f03e188d
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-22 00:27:27 +0530

    ❓

--- IF Else/21_ifElse_Que1.cpp
+++ IF Else/21_ifElse_Que1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 { // Area of circle vs Perimeter
-    float r;
+    float R;
     cout << "Enter Radius : ";
-    cin >> r;
-    float Area = 3.14 * r * r;
-    float Perimeter = 3.14 * 2 * r;
+    cin >> R;
+    float Area = 3.14 * R * R;
+    float Perimeter = 3.14 * 2 * R;
     cout << "Area of circle : " << Area<<endl;
     cout << "Perimeter of circle : " << Perimeter<<endl;
     if (Area > Perimeter)
