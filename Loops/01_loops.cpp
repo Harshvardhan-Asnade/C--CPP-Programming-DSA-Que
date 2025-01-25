f1c750282ab82dd8ee8aedbe24ad7dda357f0a22
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-25 15:13:55 +0530

    Add loops

---
+++ Loops/01_loops.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{ // i want print the welcome to ai world 11 times using loops
+ for(int i = 1 ;i <= 10; i++)
+    {   
+        cout << "Welcome To AI World\n";
+    }
+}
