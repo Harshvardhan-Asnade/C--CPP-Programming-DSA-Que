d5c26c3ee3dd9754bb776d28c6105ab64daae086
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-25 15:18:55 +0530

    Que 01

---
+++ Loops/02_loops_Que1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ // i want print the welcome to ai world  n times using loops take  n form user
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Welcome To AI World\n";
+    }
+}
