f0c1d5203e01ab0130400da0034a787e50da32cd
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-25 16:16:38 +0530

    Que7

---
+++ Loops/08_loops_Que8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ //100,97,94
+     
+    int i, a = 100;
+    for (i = 1; i <=a; i--)
+    {  if(a>0)
+        cout << a << endl;
+        a = a -3;
+    }
+}
