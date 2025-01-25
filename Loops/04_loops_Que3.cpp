9fd8f167d7f2b81fd65d0a4cbaf1ec5962fa088e
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-25 15:35:01 +0530

    Que 0

---
+++ Loops/04_loops_Que3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{ //  print 1 to 100 EVEN NUMBER
+    int i;
+    for (i = 0; i < 100;)
+    {
+        i = i + 2;
+        cout << i << endl;
+    }
+}
