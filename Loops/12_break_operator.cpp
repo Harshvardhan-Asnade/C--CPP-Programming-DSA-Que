eda36ef507a72152b031398ac35028d01e4b133c
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-01-26 09:13:50 +0530

    break Oprator

---
+++ Loops/12_break_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int n ;
+  cout<<"Enter a Number : ";
+  cin>>n;
+  for (int i = 2; i <=n-1; i++)
+  {
+    if (n%i==0)         
+    {
+        cout<<n<<" is a composite number"<<endl;
+        break;
+    }
+    
+  }
+  
+}  
