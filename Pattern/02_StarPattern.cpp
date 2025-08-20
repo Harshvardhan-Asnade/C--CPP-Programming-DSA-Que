b65a13c52f11f1c1a123da9f66cc4b0d86751cb0
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-08-20 21:27:17 +0530

    fix star Pattern

--- Pattern/02_StarPattern.cpp
+++ Pattern/02_StarPattern.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int n;
-    cout << "Enter The Vaule : ";
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            cout << "*";
+ #include <iostream>
+ using namespace std;
+ int main(){
+    
+    for(int i =1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
-    return 0 ;
-}
+ }
