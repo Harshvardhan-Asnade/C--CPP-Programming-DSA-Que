b9f1e5a0c3977ca770c3a49eb2d94cbe3fc545e1
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-11-22 18:46:10 +0530

    Printing Arrays Using Functns Throught Pointrs

---
+++ Arrays /07_PrintArrayUsingFuctions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void PrintingAarry(int arr[],int n){
+for (int i = 0; i <n; i++)
+{
+    cout<<arr[i]<<endl;
+}
+}
+int main(){
+int arr[]={1,2,3,4,5};
+int n =sizeof(arr)/sizeof(int);
+PrintingAarry(arr,n);
+return 0;
+}
