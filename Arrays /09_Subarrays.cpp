43597e3769fd765c24c339b755929406e33599b2
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-11-23 21:06:19 +0530

    Subarrays

---
+++ Arrays /09_Subarrays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] ={1,2,3,4,5};
+    int n =sizeof(arr)/sizeof(int);
+    for(int start = 0 ; start <= n-1; start++){
+    for(int end = start; end <= n-1; end++){
+        for(int i = start; i <= n-1; i++) 
+            cout << arr[i];
+    }
+    cout << endl; 
+}
+}
