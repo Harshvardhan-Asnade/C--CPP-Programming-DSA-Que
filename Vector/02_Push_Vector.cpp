70041ca24c1a04f19dd0521004b2a0d94ddb0fc4
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-12-04 15:41:51 +0530

    Vector

---
+++ Vector/02_Push_Vector.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v ;
+    v.push_back(10);
+    v.push_back(2);
+    v.push_back(8);
+    cout<<v[1]<<endl;    
+    cout<<v[0]<<endl;  
+    cout<<v[2]<<endl;  
+    cout<<v[3]<<endl;  
+}        
