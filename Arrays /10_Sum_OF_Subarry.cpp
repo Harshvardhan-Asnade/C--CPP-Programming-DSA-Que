a322ad0c5c5664f15f2982fbd8a6a4bf97be5dbc
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-12-20 15:32:22 +0530

    Arrays /10_Sum_OF_Subarry.cpp

--- Arrays /10_Sum_OF_Subarry.cpp
+++ Arrays /10_Sum_OF_Subarry.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
 
+    int maxSum = INT_MIN;
+
     for (int start = 0; start < n; start++) {
         for (int end = start; end < n; end++) {
             int CurrSum = 0;
 
             for (int i = start; i <= end; i++) {
-                
                 CurrSum += arr[i];
             }
 
-            cout << "=> Sum = " << CurrSum << endl;
+            maxSum = max(maxSum, CurrSum);
         }
-        cout << endl;
     }
+
+    cout << "Maximum Subarray Sum = " << maxSum << endl;
 
     return 0;
 }
