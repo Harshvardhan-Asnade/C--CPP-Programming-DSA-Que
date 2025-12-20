02e8bfbf6163d4c67208b6c8403dafa5559bcf19
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-12-20 16:13:08 +0530

    Arrays /11_KadanesAlgorithm.cpp

---
+++ Arrays /11_KadanesAlgorithm.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<climits>
+using namespace std ;
+int main(){
+      int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    int MAXSUM =INT_MIN;
+    int CurrSum=0;
+    for(int i=0;i<n;i++){
+        CurrSum = CurrSum+arr[i];
+        MAXSUM=max(CurrSum,MAXSUM);
+    } cout<<MAXSUM;
+}
